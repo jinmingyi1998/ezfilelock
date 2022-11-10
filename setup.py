0ca3e955f81a9ed57fd5ece23ea6a3c0f3464eda
Mingyi Jin <jinmingyi1998@sina.cn>
2022-11-10 17:32:26 +0800

    add project pypi description

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
+from pathlib import Path
+
 exec(open('ezfilelock/version.py').read())
+long_description = Path("README.md").read_text()
 setup(
     name='ezfilelock',
     version=__version__,
@@ -8,5 +11,7 @@ setup(
     license='MIT License',
     author='Mingyi Jin',
     author_email='jinmingyi1998@sina.cn',
-    description='A simple cross-platform file lock'
+    description='A simple cross-platform file lock',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
